4f81ed32fa4477060b0c6f4eb0389e8b2a2ff398
Sujeet Kumar Mohanty <60059829+sujeetmohanty11@users.noreply.github.com>
2020-03-01 08:18:09 +0530

    Add files via upload

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Sujeet Kumar Mohanty on 01/03/2020.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
